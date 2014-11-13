8bb44ecb69a9fba97a92235c86d9ed0ed0f4fa1c
Rafael <rafastdb@hotmail.com>
2014-11-13 16:11:18 -0200

    oooooo

--- EmporiumSystem 0.01/EmporiumSystem 0.01/Form1.cs
+++ EmporiumSystem 0.01/EmporiumSystem 0.01/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using MetroFramework.Fonts;
 using MetroFramework.Forms;
 using MetroFramework;
-
+using Transitions;
 
 
 namespace EmporiumSystem_0._01
@@ -23,10 +23,10 @@ namespace EmporiumSystem_0._01
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {       
-          //  Transition t1 = new Transition(new TransitionType_Linear(1000));
-            //t1.add(pictureBox1, "Top", 80);
-          //  Transition.runChain(t1);
+       {
+        //    Transition t1 = new Transition(new TransitionType_Linear(1000));
+        //    t1.add(pictureBox1, "Top", 80);
+        //    Transition.runChain(t1);
 
             metroToolTip1.SetToolTip(metroTextBox1, "Digite seu nome de usuário");             
         }
